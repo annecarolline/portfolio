3414871effa7fa420a12d8ea8b2e083eaca29dae
Anne Carolline Azevedo <annecarolline.azevedo@yahoo.com.br>
2026-02-22 15:31:51 -0300

    :recycle: reactor: Refeita a secao de graficos, e add mais um grafico com outras habilidades.

--- js/script.js
+++ js/script.js
@@ -18,59 +18,86 @@ function mudaCorTema(){
 document.querySelector('#muda-tema-cor').addEventListener('click',mudaCorTema);
 //document.querySelector('#muda-tema-cor').removeEventListener('click',mudaCorTema);
 
+function criarOuAtualizarGraf(dadosChart,idChart) {
+    const chartStatus = Chart.getChart(idChart); // Get existing chart instance by canvas ID
 
-const ctx = document.getElementById('myChart');
-const labels = ['Javascript','MySQL','PHP','Postgres','JQuery','CSS','Git'];
-const dados = {
-    labels: labels,
+    if (chartStatus !== undefined) {
+        chartStatus.destroy(); // Destroy it if it exists
+    }
+
+    let delayed;
+
+    // Create a new chart
+    const ctx = document.getElementById(idChart).getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'pie',
+        data: dadosChart,
+        options: {
+            animation: {
+                onComplete: () => {
+                    delayed = true;
+                },
+                delay: (context) => {
+                    let delay = 0;
+                    if (context.type === 'data' && context.mode === 'default' && !delayed) {
+                        delay = context.dataIndex * 300 + context.datasetIndex * 100;
+                    }
+                    return delay;
+                },
+            }
+        }
+    });
+}
+
+const labels1 = ['Javascript','MySQL','PHP','Postgres','JQuery','CSS','Git','Composer','Outras'];
+const dados1 = {
+    labels: labels1,
     datasets: [{
-        label: 'Principais habilidades técnicas',
-        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Principais habilidades em %',
+        data: [12, 16, 18, 18, 12, 10, 8, 6, 2],
         backgroundColor: [
-        'rgba(208, 73, 212, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
+        'rgba(208, 73, 212, 0.7)',
+        'rgba(255, 205, 86, 0.7)',
+        'rgba(153, 102, 255, 0.7)',
+        'rgba(75, 192, 192, 0.7)',
+        'rgba(255, 99, 132, 0.7)',
+        'rgba(54, 162, 235, 0.7)',
+        'rgba(255, 159, 64, 0.7)',
+        'rgba(215, 59, 46, 0.7)',
+        'rgba(34, 259, 94, 0.7)'
         ],
-        borderColor: [
-        'rgba(227, 162, 243, 1)',
-        'rgb(255, 205, 86)',
-        'rgb(153, 102, 255)',
-        'rgb(75, 192, 192)',
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 159, 64)'
-        ],
+        borderColor: '#fff',
         borderWidth: 1
     }]
 }
 
-let delayed;
+const labels2 = ['React.js','Vue.js','Oracle','SQL Server','Docker','Python','C#','Node','Redis','MongoDB','Postman','PhpUnit','Google Cloud','NPM'];
+const dados2 = {
+    labels: labels2,
+    datasets: [{
+        label: 'Outras habilidades em %',
+        data: [7, 7, 7, 8, 7, 7, 7, 7, 7, 7, 8, 7, 7, 7],
+        backgroundColor: [
+        '#FF6467',
+        '#FF8904',
+        '#FFB93B',
+        '#FDC745',
+        '#9AE630',
+        '#05DF72',
+        '#31D492',
+        '#38D5BE',
+        '#42D3F2',
+        '#21BCFF',
+        '#51A2FF',
+        '#7C86FF',
+        '#A684FF',
+        '#C27AFF',
+        '#E12AFB'
+        ],
+        borderColor: '#fff',
+        borderWidth: 1
+    }]
+}
 
-new Chart(ctx, {
-    type: 'bar',
-    data: dados,
-    options: {
-        indexAxis: 'y',
-        animation: {
-            onComplete: () => {
-                delayed = true;
-            },
-            delay: (context) => {
-                let delay = 0;
-                if (context.type === 'data' && context.mode === 'default' && !delayed) {
-                    delay = context.dataIndex * 300 + context.datasetIndex * 100;
-                }
-                return delay;
-            },
-        },
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+criarOuAtualizarGraf(dados1,'chart1');
+criarOuAtualizarGraf(dados2,'chart2');
